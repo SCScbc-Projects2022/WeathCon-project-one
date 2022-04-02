95680a9dd190fe0ea53cbbdb6f61406b3c5a8f96
Candice Hall <hall.candice@outlook.com>
2022-04-02 13:50:52 -0400

    added new weather api + checked functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,5 +5,24 @@ var containerThree = document.querySelector("#container-3");
 // departure will be current location, destination will be search location
 //Brennan's code here
 //Candice's code here
+// var city = "toronto";
+var getWeather = function(city){
+    var apiURL = "https://api.weatherbit.io/v2.0/forecast/daily?units=I&days=15&city=" + city + "&key=023238f0f4fa4eb49d76fc4a4b8d5c55";
+    fetch(apiURL)
+    .then(function(response){
+        if (response.ok){
+            response.json().then(function(data){
+                // displayWeather(data);
+                console.log(data);
+            })
+//         } else {
+//             //insert error handling here
+         };
+    })
+}
+// var displayWeather = function(data){
+
+// }
+getWeather();
 //Veronica's code here
 //Cory's code here
