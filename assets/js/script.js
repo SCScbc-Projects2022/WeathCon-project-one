c787843a8c69f5c71f3c97a4785bea794abb91d5
Candice Hall <hall.candice@outlook.com>
2022-04-03 19:10:39 -0400

    new api

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,32 +7,34 @@ var containerThree = document.querySelector("#container-3");
 //Candice's code here
 // var city = "toronto";
 var getWeather = function(city){
-    var apiURL = "https://api.weatherbit.io/v2.0/forecast/daily?units=I&days=15&city=ottawa&key=023238f0f4fa4eb49d76fc4a4b8d5c55";
+    console.log(city);
+    var apiURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?locations=" + city + "&aggregateHours=24&forecastDays=15&unitGroup=metric&shortColumnNames=false&contentType=json&iconSet=icons1&key=DDEWS835GJQFSW9E6Z6B3TS3K";
     fetch(apiURL)
     .then(function(response){
         if (response.ok){
             response.json().then(function(data){
-                 displayWeather(data);
                 console.log(data);
+                  displayWeather(data, city);
+                
             })
          } else {
              //insert error handling here
          };
     })
 }
- var displayWeather = function(data){
-
+ var displayWeather = function(data, city){
+var cityName = city;
 var weatherTitleEl = document.createElement("div");
-weatherTitleEl.textContent=data.city_name;
+weatherTitleEl.textContent=data.locations[cityName].address;
 containerOne.appendChild(weatherTitleEl);
 
 var currentWeather = document.createElement("div");
-currentWeather.textContent = "Temp: " + data.data[0].temp + " °C";
+currentWeather.textContent = "Temp: " + data.locations[cityName].currentConditions.temp + " °C";
 containerOne.appendChild(currentWeather);
 
-var currentIcon = document.createElement("div");
-currentIcon.innerHTML="<img src='https://www.weatherbit.io/static/img/icons/" + data.data[0].weather.icon + ".png'>"
-containerOne.appendChild(currentIcon);
+// var currentIcon = document.createElement("div");
+// currentIcon.innerHTML="<img src='https://www.weatherbit.io/static/img/icons/" + data.locations.city.currentConditions.icon + ".png'>"
+// containerOne.appendChild(currentIcon);
 
 var dropForecast = document.createElement("select");
 dropForecast.classList.add("select");
@@ -49,24 +51,38 @@ var fourteenForecast = document.createElement("option");
 fourteenForecast.textContent="14 Day Forecast";
 $(fourteenForecast).attr("id", "14");
 dropForecast.appendChild(fourteenForecast);
- }
 
- var destinationForecast = function(num){
-     for (var i = 1; i<num;i++){
+// fiveDay.addEventListener("change", function(){
+//     for(var i = 1; i < 6; i++){
+//         var destinationDayEl = document.createElement("div");
+//         destinationDayEl.classList.add("border");
+//         containerOne.appendChild(destinationDayEl);
+        
+//         var days = document.createElement("ul");
+//         destinationDayEl.appendChild(days);
+        
+//         var date = document.createElement("li");
+//         date.setAttribute("class", "days-text");
+//         date.textContent=data.locations[cityName].values[i].datetime;
+//         days.appendChild(date);
 
-     }
- }
-//  $(".select").change(function(){
-//      if($(this).id === "14"){
+//         var temp = document.createElement("li");
+//         temp.setAttribute("class", "days-text");
+//         temp.textContent= "Temp: " + data.locations[cityName].values[i].temp + " °C";
+//         days.appendChild(temp);
 
-//      }else if($(this).id === "7"){
+//         var humidity = document.createElement("li");
+//         humidity.setAttribute("class", "days-text");
+//         humidity.textContent= "Humidity: " + data.locations[cityName].values[i].humidity + "%";
+//         days.appendChild(humidity);
+//     }
+// })
+ };
+ 
 
-//      } else {
+ getWeather("toronto");
+ 
 
-//      }
-//  })
-
- getWeather();
 
 //Veronica's code here
 //Cory's code here
