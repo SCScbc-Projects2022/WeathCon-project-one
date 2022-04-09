952aa4f4e85ef4e5b5af4dbe1cbd1179a37f483c
TOVTC <veronica.tc.to@outlook.com>
2022-04-08 22:35:02 -0400

    added selection-event dependent dropdown options

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,25 +4,113 @@ $("#logo").on("click", function() {
 	document.location.replace("./index.html");
 });
 
-//drop down menus
-$(function() {
-    $("#country-selector").selectmenu();
-	$("#city-selector").selectmenu();
-    $("#country-picker").selectmenu();
-	$("#city-picker").selectmenu();
+getCountries();
+//populate country drop downs
+function getCountries() {
+    var apiUrl = "https://countriesnow.space/api/v0.1/countries/info?returns=name,cities";
+    var dataOne = fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                var data = response.json().then(function (data) {
+					for (i = 0; i < data.data.length; i++) {
+						if (data.data[i].cities) {
+							var country = data.data[i].name;
+							var option = $("<option>").attr("value", country).data("index", i).text(country);
+							$("#country-selector").append(option);
+						}
+					}
+					for (i = 0; i < data.data.length; i++) {
+						if (data.data[i].cities) {
+							var country = data.data[i].name;
+							var option = $("<option>").attr("value", country).data("index", i).text(country);
+							$("#country-picker").append(option);
+						}
+					}
+                });
+                return data;
+            } else {
+                alert("unable to retrieve location data");
+                return;
+            }
+        })
+        .catch(function (error) {
+            alert("unable to connect with location API");
+            return;
+        });
+    return dataOne;
+}
+
+function getDepartureCities(country) {
+	var apiUrl = "https://countriesnow.space/api/v0.1/countries/info?returns=name,cities,flag";
+    var dataOne = fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+				console.log(country);
+				var data = response.json().then(function (data) {
+					console.log(data.data[country].cities)
+					for (i = 0; i < data.data[country].cities.length; i++) {
+						console.log("should be generated")
+                    var city = data.data[country].cities[i];
+					var option = $("<option>").text(city);
+					$("#city-selector").append(option);
+					}
+                });
+                return data;
+            } else {
+                alert("unable to retrieve conversion data");
+                return;
+            }
+        })
+        .catch(function (error) {
+            alert("unable to connect with currency API");
+            return;
+        });
+    return dataOne;
+}
+
+function getDestinationCities(country) {
+	var apiUrl = "https://countriesnow.space/api/v0.1/countries/info?returns=name,cities,flag";
+    var dataOne = fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+				console.log(country);
+				var data = response.json().then(function (data) {
+					console.log(data.data[country].cities)
+					for (i = 0; i < data.data[country].cities.length; i++) {
+						console.log("should be generated")
+                    var city = data.data[country].cities[i];
+					var option = $("<option>").text(city);
+					$("#city-picker").append(option);
+					}
+                });
+                return data;
+            } else {
+                alert("unable to retrieve conversion data");
+                return;
+            }
+        })
+        .catch(function (error) {
+            alert("unable to connect with currency API");
+            return;
+        });
+    return dataOne;
+}
+
+$("#country-selector").on("change", function() {
+	$("#city-selector").empty();
+	getDepartureCities($("#country-selector").find(":selected").data("index"));
 });
 
-//use these placeholders if you want to test your code without it breaking
-var departureCity = "";
-var departureCountry = "";
-var destinationCity = "";
-var destinationCountry = "";
+$("#country-picker").on("change", function() {
+	$("#city-picker").empty();
+	getDestinationCities($("#country-picker").find(":selected").data("index"));
+});
 
 $("#initialSubmit").on("click", function() {
-	departureCity = $("#city-selector").val();
-	departureCountry = $("#country-selector").val();
-	destinationCity = $("#city-picker").val();
-	destinationCountry = $("#country-picker").val();
+	var departureCity = $("#city-selector").val();
+	var departureCountry = $("#country-selector").val();
+	var destinationCity = $("#city-picker").val();
+	var destinationCountry = $("#country-picker").val();
 	if (!departureCity || !departureCountry || !destinationCity || !destinationCountry) {
 		alert("please enter valid departure and destination locations");
 	} else {
